cdbddfe82ce0601de7b1d363f1f70e70b8cafa62
LeonardoRangel12 <l20211830@tectijuana.edu.mx>
2023-10-31 18:37:53 -0700

    Multiples variables funcional

--- exptree.c
+++ exptree.c
@@ -3,91 +3,111 @@
 #include <string.h>
 #include <stdbool.h>
 #include "exptree.h"
-extern SymbolTable* head; // Head of the Symbol Table
-//Methods for Symbol Table
-SymbolTable *getSymbol (char *name )
+SymbolTable *head = NULL;
+// Methods for Symbol Table
+SymbolTable *getSymbol(char *name)
 {
     SymbolTable *ptr;
-    for (ptr = head; ptr != (SymbolTable *) 0;ptr = (SymbolTable *)ptr->next){
-        if (strcmp (ptr->name,name) == 0){
+    for (ptr = head; ptr != (SymbolTable *)0; ptr = (SymbolTable *)ptr->next)
+    {
+        if (strcmp(ptr->name, name) == 0)
+        {
             return ptr;
         }
     }
     return (SymbolTable *)0;
 }
-SymbolTable *putSymbol ( char* name, char* type)
+SymbolTable* putSymbol(char *name, char *type)
 {
-    
-    SymbolTable *ptr;
-    ptr = (SymbolTable *) malloc (sizeof(SymbolTable));
-    ptr->name = (char *) malloc (strlen(name)+1);
-    strcpy (ptr->name,name);
-    ptr->type = (char *) malloc(strlen(type)+1);
-    strcpy(ptr->type,type);
-    ptr->next = (struct SymbolTable *)head;
-    if(head != NULL){
-        return head->next = ptr;
+    SymbolTable *nuevo = malloc(sizeof(SymbolTable));
+    nuevo->name = name;
+    nuevo->type = type;
+    nuevo->next = NULL;
+    if (head == NULL)
+    {
+        head = nuevo;
+        return head;
     }
-    else{
-        head = ptr;
-        return ptr;
-    }
-}
+    else
+    {
+        SymbolTable *tmp = head;
+        while (tmp->next != NULL)
+        {
+            tmp = tmp->next;
+        }
 
-void printSymbol(){ // Print the Symbol Table
-    SymbolTable *temp = head;
-    while(temp != NULL){
-        printf("%s = %s\n", temp->name, temp->type);
-        temp = temp->next;
+        // insertar al final
+        tmp->next = nuevo;
+        return tmp->next;
     }
 }
-//Delete a variable from the Symbol Table
-void deleteFromSymbolTable(){
-    if(head == NULL){
-        return;
-    }
-    else{
+    void printSymbol()
+    { // Print the Symbol Table
         SymbolTable *temp = head;
-        head = head->next;
-        free(temp);
-    }
-}
-//Delete the whole Symbol Table
-void deleteSymbolTable(){
-    while(head != NULL){
-        deleteFromSymbolTable();
-    }
-}
-// Create a new node for the AST without Type
-TreeNode* createNode(char* data) {
-    TreeNode* newNode = (TreeNode*) malloc(sizeof(TreeNode)); // Allocate memory for the new node
-    if (newNode) {// Initialize the new node
-        newNode->data = (char*) malloc(sizeof(data)+1);
-        if(newNode->data != NULL){
-            strcpy(newNode->data, data);
-            newNode->type = NULL;
-            newNode->left = NULL;
-            newNode->right = NULL;
+        while (temp != NULL)
+        {
+            printf("%s = %s\n", temp->name, temp->type);
+            temp = temp->next;
         }
     }
-    return newNode;
-}
-// Function to print the AST in prefix notation
-void printAST(TreeNode* root) {
-    if (root != NULL) {
-        printf("%s ", root->data);
-        printAST(root->left);
-        printAST(root->right);
+    // Delete a variable from the Symbol Table
+    void deleteFromSymbolTable()
+    {
+        if (head == NULL)
+        {
+            return;
+        }
+        else
+        {
+            SymbolTable *temp = head;
+            head = head->next;
+            free(temp);
+        }
     }
-}
-// Function to free the memory of the AST
-void freeAST(TreeNode* root) {
-    if (root != NULL) {
-        freeAST(root->left);
-        freeAST(root->right);
-        free(root->data);
-        free(root->type);
-        free(root);
+    // Delete the whole Symbol Table
+    void deleteSymbolTable()
+    {
+        while (head != NULL)
+        {
+            deleteFromSymbolTable();
+        }
     }
-}
-
+    // Create a new node for the AST without Type
+    TreeNode *createNode(char *data)
+    {
+        TreeNode *newNode = (TreeNode *)malloc(sizeof(TreeNode)); // Allocate memory for the new node
+        if (newNode)
+        { // Initialize the new node
+            newNode->data = (char *)malloc(sizeof(data) + 1);
+            if (newNode->data != NULL)
+            {
+                strcpy(newNode->data, data);
+                newNode->type = NULL;
+                newNode->left = NULL;
+                newNode->right = NULL;
+            }
+        }
+        return newNode;
+    }
+    // Function to print the AST in prefix notation
+    void printAST(TreeNode * root)
+    {
+        if (root != NULL)
+        {
+            printf("%s ", root->data);
+            printAST(root->left);
+            printAST(root->right);
+        }
+    }
+    // Function to free the memory of the AST
+    void freeAST(TreeNode * root)
+    {
+        if (root != NULL)
+        {
+            freeAST(root->left);
+            freeAST(root->right);
+            free(root->data);
+            free(root->type);
+            free(root);
+        }
+    }
